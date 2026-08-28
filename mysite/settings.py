c666e743966b8a5c47bb9858b7c85f151e05c490
Matilainen Tuuli K <matuuli@cubbli24-gold.cs.helsinki.fi>
2026-08-28 17:48:26 +0300

    Last fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,9 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vw(r=zhd+owya13=tk8@!v63pna+vyntyp090vid(fm*#ond_9"
+import os
 
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "development-only-key"
+)
 
 #vulnerable version:
 DEBUG = True
